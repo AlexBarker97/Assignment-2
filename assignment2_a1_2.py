9e804cd5e20c26b3accd47db222c387890a0f278
AlexBarker97 <A.Barker.1@Warwick.ac.uk>
2017-03-06 10:36:13 +0000

    Update assignment2_a1_2.py

--- assignment2_a1_2.py
+++ assignment2_a1_2.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 14 15:11:37 2017
-
-@author: shreejiths
-"""
 #Python Template for ES2B4 Assignment - 2
 
 #Name: Alex Barker
@@ -12,9 +5,10 @@ Created on Tue Feb 14 15:11:37 2017
 #Assignment Section : assignment_a1_2
 
 ######## Your Code Below ########
+import math
+
 def Tangent(AMin,AMax):
     '''Prints calues for the tangent of an angle in degrees'''
-    import math
     Angles=[x for x in range(AMin,AMax+1)]
     Sin = []
     Cos = []
@@ -29,7 +23,7 @@ def Tangent(AMin,AMax):
         Tan.append((Sin[i]/10000000)/(Cos[i]/10000000))
         i+=1
     print("Tangent of", AMin, "to", AMax,"degrees in steps of 1 degree: ")
-    print(Tan)
+    return(Tan)
 
 def VowelsExtract(INPUT):
     '''Prints a list of the vowles found in an input given in the form: \n VowelsExtract(input to check)'''
@@ -42,7 +36,7 @@ def VowelsExtract(INPUT):
         if INPUT[i] in Vowels:
             VowelsFound.append(INPUT[i])
         i+=1
-    print("Vowels Found: ",VowelsFound)
+    return("Vowels Found: ",VowelsFound)
 
 def xx5(Xmin, Xmax, Ymax):
     '''Prints values for y=x*x+5 in ranges given using inputs:\n xx5(lower x, upper x, upper y) \n note, lower y default 0'''
@@ -67,10 +61,10 @@ def xx5(Xmin, Xmax, Ymax):
         x.remove(x[i-count])
         count+=1
     for i in x:
-        print(i,(i*i+5))
+        return(i,(i*i+5))
 
-Tangent(0,89)
+print(Tangent(0,89))
 print("\n")
-VowelsExtract("Alex Barker Assignment 2")
+print(VowelsExtract("Alex Barker Assignment 2"))
 print("\n")
-xx5(-5,5,10)
+print(xx5(-5,5,10))
