c1e612bff0f6b7b97b4307b1ffcf135e6d234184
AlexBarker97 <A.Barker.1@Warwick.ac.uk>
2017-03-06 10:28:47 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 14 15:11:37 2017
-
-@author: shreejiths
-"""
 #Python Template for ES2B4 Assignment - 2
 
 #Name: Alex Barker
